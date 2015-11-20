3b8f9e59b0a7a2da15e2cae40bae870c9bc9828d
lll <tkk0318@naver.com>
2015-11-20 19:00:28 +0900

    Factory and puss

--- src/kr/talenton/web/dao/CommentDao.java
+++ src/kr/talenton/web/dao/CommentDao.java
@@ -3,16 +3,16 @@ package kr.talenton.web.dao;
 import java.sql.SQLException;
 import java.util.List;
 
-import kr.talenton.web.dao.Comment;
+import kr.talenton.web.vo.Comment;
 
 public interface CommentDao {
-	public List<Member> getMembers() throws SQLException;
-	public List<Member> getMembers(int page) throws SQLException;
-	public List<Member> getMembers(int page,String field, String query) throws SQLException;
+	public List<Comment> getComments() throws SQLException;
+	public List<Comment> getComments(int more_count) throws SQLException;
+	public List<Comment> getComments(int page,String field, String query) throws SQLException;
 
 	
-	public int update(Member member) throws SQLException;
-	public int delete(String mid) throws SQLException;
-	public int insert(Member member) throws SQLException;
+	public int update(Comment comment) throws SQLException;
+	public int delete(String member_id, int product_code) throws SQLException;
+	public int insert(Comment comment) throws SQLException;
 	
 }
--- src/kr/talenton/web/dao/MyBatisCommentDao.java
+++ src/kr/talenton/web/dao/MyBatisCommentDao.java
@@ -1,5 +1,79 @@
 package kr.talenton.web.dao;
 
-public class MyBatisCommentDao {
+import java.sql.SQLException;
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import kr.talenton.web.dao.CommentDao;
+import kr.talenton.web.dao.mybatis.TalentOnSqlSessionFactoryBuilder;
+import kr.talenton.web.vo.Comment;
+
+public class MyBatisCommentDao {
+	SqlSessionFactory ssf = TalentOnSqlSessionFactoryBuilder.getSqlSessionFactory();
+	//멤버 다오를 공장에서 꺼내와야함
+	
+	@Override
+	public List<Member> getMembers() throws SQLException {
+		// TODO Auto-generated method stub
+		return getMembers(1,"MID","");
+	}
+
+	@Override
+	public List<Member> getMembers(int page) throws SQLException {
+		// TODO Auto-generated method stub
+		return getMembers(page,"MID","");
+	}
+
+	@Override
+	public List<Member> getMembers(int page, String field, String query) throws SQLException {
+		// TODO Auto-generated method stub
+		SqlSession session = ssf.openSession();
+		MemberDao dao = session.getMapper(MemberDao.class);
+		
+		List<Member> list = dao.getMembers(page, field, query);
+		session.close();
+		
+		return list;
+	}
+
+	@Override
+	public int update(Member member) throws SQLException {
+		// TODO Auto-generated method stub
+		SqlSession session = ssf.openSession();
+		MemberDao dao = session.getMapper(MemberDao.class);
+		int aft = dao.update(member);
+		
+		session.commit();		
+		session.close();
+		
+		return aft;
+	}
+
+	@Override
+	public int delete(String mid) throws SQLException {
+		// TODO Auto-generated method stub
+		SqlSession session = ssf.openSession();
+		MemberDao dao = session.getMapper(MemberDao.class);
+		int aft = dao.delete(mid);
+		
+		session.commit();
+		session.close();
+		
+		return aft;
+	}
+
+	@Override
+	public int insert(Member member) throws SQLException {
+		// TODO Auto-generated method stub
+		SqlSession session = ssf.openSession();
+		MemberDao dao = session.getMapper(MemberDao.class);
+		int aft = dao.insert(member);
+		
+		session.commit();		
+		session.close();
+		
+		return aft;
+	}
 }
