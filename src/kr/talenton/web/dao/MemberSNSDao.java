cf7bd0b0743163ed30759472e6d2fbcc8057dda2
SH <sang4196@naver.com>
2015-11-20 19:56:02 +0900

    jwkimNeo 20151120 1955

--- src/kr/talenton/web/dao/MemberSNSDao.java
+++ src/kr/talenton/web/dao/MemberSNSDao.java
@@ -6,7 +6,7 @@ import kr.talenton.web.vo.MemberSNS;
 
 public interface MemberSNSDao {
 	
-	public List<MemberSNS> getMemberSNSs();
+	public List<MemberSNS> getMemberSNSs(String member_id, String name);
 	public int update(MemberSNS membersns);
 	public int delete(String member_id, String name);
 	public int insert(MemberSNS membersns);
