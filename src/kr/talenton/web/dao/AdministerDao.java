0f295bfc899099c4081f66122677b90ff12cf185
lll <bigggunt93@naver.com>
2015-11-20 18:28:06 +0900

    administerdao 수정(ysi)

--- src/kr/talenton/web/dao/AdministerDao.java
+++ src/kr/talenton/web/dao/AdministerDao.java
@@ -1,5 +1,18 @@
 package kr.talenton.web.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.talenton.web.vo.Administer;
+
+
 public interface AdministerDao {
+	
+	public List<Administer> getMembers() throws SQLException;
+	public List<Administer> getMembers(int page) throws SQLException;
+	public List<Administer> getMembers(int page, String field, String query) throws SQLException;
+	public int update(Administer administer) throws SQLException;
+	public int delete(String mid) throws SQLException;
+	public int insert(Administer member) throws SQLException;
 
 }
